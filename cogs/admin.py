e57314fdf5fc38ec3a1c6a362ddc40c1e13c3342
SirThane <jerade.hensley@gmail.com>
2017-08-25 21:58:29 -0400

    Added

--- cogs/admin.py
+++ cogs/admin.py
@@ -7,8 +7,80 @@ from discord.ext import commands
 import discord
 import asyncio
 import logging
+import os
+from .utils import checks
 
 log = logging.getLogger()
+
+
+class GuildConverter(commands.IDConverter):
+    """Converts to a :class:`Guild`.
+
+    All lookups are done via global guild cache.
+
+    The lookup strategy is as follows (in order):
+
+    1. Lookup by ID
+    2. Lookup by name
+    """
+    async def convert(self, ctx, argument):
+        bot = ctx.bot
+        match = self._get_id_match(argument)
+
+        if match is None:
+            result = discord.utils.get(bot.guilds, name=argument)
+        else:
+            guild_id = int(match.group(1))
+            result = bot.get_guild(guild_id)
+
+        if not isinstance(result, discord.Guild):
+            raise commands.BadArgument('Guild "{}" not found.'.format(argument))
+
+        return result
+
+
+class UnspecifiedConverter(commands.IDConverter):
+    """Converts to any of the following:
+    :class:`Guild`, :class:`TextChannel`, :class:`VoiceChannel`,
+    :class:`Role`, :class:`Member`, :class:`User`
+
+    all lookups are done via both local and global caches
+
+    the lookup strtegy is as follows (in order):
+
+    1. Guild
+    2. Role
+    3. TextChannel
+    4. VoiceChannel
+    5. Member
+    6. User
+    7. Emoji
+    """
+    async def convert(self, ctx, argument):
+        bot = ctx.bot
+        converters = [
+            GuildConverter,
+            commands.converter.RoleConverter,
+            commands.converter.TextChannelConverter,
+            commands.converter.VoiceChannelConverter,
+            commands.converter.MemberConverter,
+            commands.converter.UserConverter,
+            commands.converter.EmojiConverter,
+        ]
+        result = None
+
+        for converter in converters:
+            try:
+                instance = converter()
+                result = await instance.convert(ctx, argument)
+                break
+            except (commands.BadArgument, commands.NoPrivateMessage):
+                continue
+
+        if result is None:
+            raise commands.BadArgument(f'Item "{argument}" not found.')
+
+        return result
 
 
 class Admin:
@@ -26,6 +98,11 @@ class Admin:
             await ctx.message.edit(embed=discord.Embed(title=message, color=color))
         await asyncio.sleep(3)
         await resp.delete()
+
+    def pull(self):
+        resp = os.popen("git pull").read()
+        resp = f"```diff\n{resp}\n```"
+        return resp
 
     @commands.command(hidden=True)
     async def load(self, ctx, *, cog: str):
@@ -62,34 +139,24 @@ class Admin:
         else:
             await self.response(ctx, 'Module reloaded.', 0x00FF00)
 
-    # @commands.command(hidden=True, name='set_config')
-    # async def _set_config(self, ctx, key: str, value: str):
-    #     """Directly alter a config file."""
-    #     try:
-    #         try:
-    #             value = int(value)
-    #         except ValueError:
-    #             pass
-    #         await self.config.put(key, value)
-    #         await ctx.message.edit(content="Success.")
-    #     except KeyError:
-    #         raise commands.CommandInvokeError
-    #
-    # @commands.command(hidden=True, name='append_to_config')
-    # async def _append_to_config(self, ctx, key: str, value: str):
-    #     """Append a value to a list in a config file"""
-    #     try:
-    #         temp_list = await self.config.get(key)
-    #         temp_list.append(value)
-    #         await self.config.put(key, temp_list)
-    #         await ctx.message.edit(content="Success.")
-    #     except KeyError:
-    #         await ctx.message.edit(content="Key {} not found.".format(key))
+    @commands.command(hidden=True)
+    async def superget(self, ctx, *, arg):
+        obj = await UnspecifiedConverter().convert(ctx, arg)
+        await ctx.send(f"{type(obj).__name__}: {obj}")
 
-    @commands.command(hidden=True, aliases=["rip", "F", "f"])
-    async def kill(self, ctx):  # Overwrites builtin kill()
+    @checks.sudo()
+    @commands.command(name="pull", hidden=True)
+    async def _pull(self, ctx):
+        resp = self.pull()
+        await ctx.send(resp)
+
+    @checks.sudo()
+    @commands.command(hidden=True, name='restart', aliases=["kill", "f"])
+    async def _restart(self, ctx, *, arg=None):  # Overwrites builtin kill()
         log.warning("Restarted by command.")
-        await ctx.message.edit(content="Restarting.")
+        if arg.lower() == "pull":
+            resp = self.pull()
+        await ctx.send(content=f"{resp}\nRestarting by command. . .")
         await self.bot.logout()
 
 
