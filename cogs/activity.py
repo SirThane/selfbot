edf87e8ecf6e6be0ce9e258fb030d45541124945
SirThane <jerade.hensley@gmail.com>
2017-09-16 00:25:11 -0400

    More changes, plus dummy methods for config commands.

--- cogs/activity.py
+++ cogs/activity.py
@@ -1,43 +1,45 @@
 import asyncio
 import discord
-# import operator
-# import math
 import datetime
 from discord.ext import commands
+from .utils import checks
+# import operator
+# import math
 
 
 """
 <bot>:activity                                              # NAMESPACE for app
 
-<bot>:activity:config                                       # HASH for global defaults (used if guild default not set)
+<ns>:config                                                 # HASH for global defaults (used if guild default not set)
     cooldown_default                                        # Default cooldown duration
     pad_amount_default                                      # Default amount of message to pad new users
+                                                            # 0 to disable
     score_period_default                                    # Default period for which messages count towards score
     prune_period_default                                    # Default period beyond which messages are pruned out of DB
     
-<bot>:activity:guilds                                       # SET for IDs of guilds being watched
+<ns>:guilds                                                 # SET for IDs of guilds being watched
 
-<bot>:activity:guilds                                       # NAMESPACE for guilds
+<ns>:guilds                                                 # NAMESPACE for guilds
 
-<bot>:activity:guilds:<guild_id>                            # HASH for guild configuration
+<ns>:guilds:<guild_id>                                      # HASH for guild configuration
     cooldown                                                # Cooldown duration
-    pad_enabled                                             # State of padding
-    pad_channel                                             # ID of channel padding messages are recorded under
+    weight                                                  # Default weight
     pad_amount                                              # Number of messages new members will be padded with
+                                                            # 0 to disable
     score_period                                            # Number of days for which messages count towards score
     prune_period                                            # Number of days beyond which messages are pruned out of DB
 
-<bot>:activity:guilds:<guild_id>                            # NAMESPACE for guild message records
+<ns>:guilds:<guild_id>                                      # NAMESPACE for guild message records
 
-<bot>:activity:guilds:<guild_id>:cooldown                   # HASH for channel specific cooldowns
+<ns>:guilds:<guild_id>:cooldown                             # HASH for channel cooldowns
 
-<bot>:activity:guilds:<guild_id>:weights                    # HASH for channel weights
+<ns>:guilds:<guild_id>:weights                              # HASH for channel weights
 
-<bot>:activity:guilds:<guild_id>:members                    # SET of members already logged from a guild
+<ns>:guilds:<guild_id>:members                              # SET of members already logged from a guild
 
-<bot>:activity:guilds:<guild_id>:members                    # NAMESPACE for member records on a guild
+<ns>:guilds:<guild_id>:members                              # NAMESPACE for member records on a guild
 
-<bot>:activity:guilds:<guild_id>:members:<member_id>:<channel_id>  # ZSET to store message records. UNIX timestamp score
+<ns>:guilds:<guild_id>:members:<member_id>:<channel_id>     # ZSET to store message records. UNIX timestamp score
                                                             # with message snowflake as value
 """
 
@@ -78,7 +80,7 @@ class ActivityMonitor:
                 recorded_members[int(guild)].add(int(member))
         return recorded_members
 
-    def pad_enabled(self, guild_id):
+    def pad_enabled(self, guild_id):  # TODO: REMOVE AND USE PAD AMOUNT
         try:
             pad_enabled = self._pad_enabled[guild_id]
         except KeyError:
@@ -90,7 +92,7 @@ class ActivityMonitor:
                 pad_enabled = False
         return pad_enabled
 
-    def get_pad_amount(self, guild_id):
+    def get_pad_amount(self, guild_id):  # TODO: MAKE SURE IT'S INT. if '0' IS True
         try:
             pad_amount = self._pad_amount[guild_id]
         except KeyError:
@@ -158,12 +160,6 @@ class ActivityMonitor:
             self.db.sadd(f"{self.ns}:guilds:{guild_id}:members", str(member_id))
             return True
 
-    # def get_current_guild_config(self, guild_id):
-    #     """Get all current configuration options for a guild."""
-    #     pass  # TODO: IS THIS REALLY NEEDED?
-
-    # Set and check cooldown
-
     async def put_cooldown(self, member_id, channel_id, dur):
         s = (member_id, channel_id)
         self.cooldown.add(s)
@@ -174,7 +170,7 @@ class ActivityMonitor:
         if member.bot:
             return True
         # s = (member.id, channel.id)
-        return (member.id, channel.id) in self.cooldown  #  or member.id == self.bot.user.id
+        return (member.id, channel.id) in self.cooldown  # or member.id == self.bot.user.id
 
     def check(self, m):
         """Returns early if in DMs, not a watched guild,
@@ -203,13 +199,18 @@ class ActivityMonitor:
         # self.bot.loop.create_task(self.put_cooldown(member, channel))
         await self.put_cooldown(member.id, channel.id, self.get_cooldown_dur(guild, channel))
 
-    async def on_message_delete(self, m):  # TODO: DELETE MESSAGE RECORD IF IT EXISTS
-        pass
+    async def on_message_delete(self, m):
+        if self.check(m):
+            return
 
-    async def on_member_remove(self, m):  # TODO: DELETE ACTIVITY RECORDS OR NAH?
-        pass
+        guild, channel, member = m.guild, m.channel, m.author
+
+        self.db.zrem(f'{self.ns}:guilds:{guild.id}:members:{member.id}:{channel.id}', str(m.id))
 
     def _pad(self, guild_id, member_id, ts):
+        """Pads a new user's points with `pad_amount`
+        message records even spaced across `score_period`
+        days in a special `pad` namespace"""
         pad_amount, score_period = self.get_pad_amount(guild_id), self.get_score_period(guild_id)
         td = datetime.timedelta(seconds=((86400 * score_period) / pad_amount))
         self.db.zadd(f'{self.ns}:guilds:{guild_id}:members:{member_id}:pad',
@@ -217,14 +218,43 @@ class ActivityMonitor:
 
     # Configuration commands
 
-    @commands.group(name='metrics', hidden=True)
-    async def metrics(self, ctx):
-        pass
+    @commands.group(name='activity', aliases=['metrics'], invoke_without_command=True)
+    async def activity(self, ctx):
+        """A system to guage activity levels of users and channels
 
-    @metrics.command(name='addguild', hidden=True)
-    async def addguild(self, ctx, *, guild: int=None):
-        if not guild:
-            guild = ctx.guild.id
+        See help page for subcommands for more details"""
+        await self.bot.formatter.format_help_for(ctx, ctx.command)
+
+    # Global config
+
+    @activity.group(name='config', invoke_without_command=True)
+    async def a_config(self, ctx):
+        """Set and view global default configurations
+
+        These configurations will be used if guild/channel
+        specific configurations are not specified.
+
+        Use this command without `set` subcommand to
+        view all current global defaults"""
+        pass  # List all global default configs
+
+    @checks.sudo()
+    @a_config.command(name='set')
+    async def a_set(self, ctx, key: str, value: int):
+        """Set a global default configuration
+
+        """
+        pass  # Set a global default config
+
+    # Guild config
+
+    @activity.group(name='guild', invoke_without_command=True)
+    async def a_guild(self, ctx):
+        pass  # List all guild configs # TODO: CREATE SUBROUTINE THAT `activity guild config` CAN ALSO CALL
+
+    @a_guild.command(name='enable')
+    async def g_enable(self, ctx):
+        guild = ctx.guild.id
         if self.db.sismember(f'{self.ns}:guilds', guild):
             g = self.bot.get_guild(id=guild)
             em = discord.Embed(title="User Metrics",
@@ -244,120 +274,31 @@ class ActivityMonitor:
                 await asyncio.sleep(5)
                 await resp.delete()
 
-    @metrics.command(name='get_report', hidden=True)
-    async def get_report(self, ctx, guild=None):
-        # TODO: Refactor the shit out of this before it ever gets off the ground
-        if not guild:
-            guild = ctx.guild
+    @a_guild.command(name='disable')
+    async def g_disable(self, ctx):
+        pass  # Enable on current guild
 
-        channels = self.db.smembers(f'{self.ns}:{guild}:channels')
-        users = self.db.smembers(f'{self.ns}:{guild}:users')
+    @a_guild.group(name='config', invoke_without_command=True)
+    async def g_config(self, ctx):
+        pass  # List all guild configs
 
-        c = {}
-        for channel in channels:
-            c[channel] = self.bot.get_channel(id=int(channel))
+    @g_config.group(name='set')
+    async def g_set(self, ctx, key: str, value: int):
+        pass  # Set a guild config
 
-        u = {}
-        for user in users:
-            u[user] = self.bot.get_member(id=int(user))
+    # Channel config
 
-        with open('test.txt', 'w') as out:
-            out.write(f'For guild: {guild.name}')
-            for user, user_id in u.items():
-                member = self.bot.get_member(id=int(user))
-                out.write(f'User: {member.name}#{member.discriminator}')
-                # for channel in channels:
-                #     pass
-
-    @metrics.group(name='set', hidden=True)
-    async def m_set(self, ctx):
+    @activity.group(name='channel')
+    async def a_channel(self, ctx):
         pass
 
-    @m_set.command(name='pad')  # TODO: CONFIGURE PAD OPTIONS
-    async def pad(self, ctx, messages: int=None, channel: discord.TextChannel=None, *, guild: discord.Guild=None):
-        return
+    @a_channel.group(name='config', invoke_without_command=True)
+    async def c_config(self, ctx, *, channel: discord.TextChannel):
+        await self.bot.formatter.format_help_for(ctx, ctx.command, "Channel must be a guild text channel.")
 
-    # @commands.command()
-    # async def tempfix(self, ctx):
-    #     for channel in ctx.guild.channels:
-    #         self.db.sadd(f'{self.ns}:{ctx.guild.id}:channels', channel.id)
-    #         await ctx.message.delete()
-
-    # Kept for reference  # TODO: REMOVE
-
-# ranks = {
-#     10: [1, "Beginner"],
-#     69: [2, "Memer Rank 1"],
-#     300: [3, "Talker"],
-#     360: [4, "MLG'er"],
-#     420: [5, "Memer Rank 2"],
-#     666: [6, "Devil"],
-#     1000: [7, "Active"],
-#     1337: [8, "1337"],
-#     3000: [9, "Really Active"],
-#     6000: [10, "Extremely Active"],
-#     9001: [11, "Memer Rank 3"],
-#     10000: [12, "Too Active"]
-# overrides = set()
-
-    # def next_ranks(self, num):
-    #     e = []
-    #     for rank in ranks.keys():
-    #         if int(rank) > int(num):
-    #             e.append(ranks[rank][1])
-    #     if len(e) == 0:
-    #         e = ["Nothing left to do!!!"]
-    #     return e
-
-    # def next_rank(self, num):
-    #     for i in range(100000):
-    #         try:
-    #             if ranks[num + i]:
-    #                 return [ranks[num + i], num + i]
-    #         except:
-    #             continue
-    #
-    #     return [[69, "Nothing"], 10000]
-
-    # @commands.command(name='rank', hidden=True, disabled=True)
-    # async def rank(self, ctx, user: discord.Member=None):
-    #     """Get someones rank"""
-    #     if user is None:
-    #         user = ctx.author
-    #     d = self.db.hget('rankcount', user.id)
-    #     if d is None:
-    #         em = discord.Embed(title="No Data", description="Try get them to speak a little more!!!")
-    #         await ctx.send(embed=em)
-    #     else:
-    #         em = discord.Embed(title=f"{str(user)}'s info")
-    #         em.set_thumbnail(url=user.avatar_url)
-    #         em.add_field(name="PyPoint count", value=f"{d} PyPoints")
-    #         em.add_field(name="Current Rank", value=self.db.hget('currank', user.id))
-    #         y = self.next_rank(int(d))
-    #         prcnt = 'nil'  # math.floor((int(d) / int(y[1])) * 100)
-    #         if y[0][0] == 69:
-    #             em.add_field(name="Next Rank", value=f"Nothing left to do")
-    #         else:
-    #             em.add_field(name="Next Rank", value=f"Level {y[0][0]} - {y[0][1]}, {str(d)}/{y[1]} ({prcnt}%) there")
-    #
-    #         await ctx.send(embed=em)
-
-    # @commands.command(name='leaderboard', hidden=True, disabled=True)
-    # async def leaderboard(self, ctx):
-    #     """Get the rank leaderboard"""
-    #     em = discord.Embed(title="Please wait", description="Compiling sources and generating leaderboard...")
-    #     message = await ctx.send(embed=em)
-    #     lb = self.db.hgetall("rankcount")
-    #     lb = {k: int(v) for k, v in lb.items()}
-    #     lb = sorted(lb.items(), key=operator.itemgetter(1), reverse=True)
-    #     _message = ""
-    #     for i in range(0, 10):
-    #         user = await self.bot.get_user_info(int(lb[i][0]))
-    #         currank = self.db.hget("currank", str(user.id))
-    #         _message += f"{i + 1}. {str(user)} - {lb[i][1]} PyPoints - Current Rank: {currank}\n"
-    #     await message.delete()
-    #     em = discord.Embed(title="Rank leaderboard", description=_message)
-    #     await ctx.send(embed=em)
+    @a_channel.command(name='set')
+    async def c_set(self, ctx, key: str, value: int):
+        pass
 
 
 def setup(bot):
